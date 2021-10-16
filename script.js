e9458be1473b84bc7e7ecfe30b01f847f36cb2e2
hugoresende27 <Hugoresende27@gmail.com>
2021-10-16 18:27:13 +0100

    sab

---
+++ script.js
@@ -0,0 +1,4 @@
+const res = document.querySelector('.output');
+const msg = document.querySelector(".msg");
+const tent = document.querySelector('input');
+const btn = document.querySelector('button');
