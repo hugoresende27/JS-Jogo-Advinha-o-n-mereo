e21ec9b3fda0fd0585a7469a4cd275b042444704
hugoresende27 <Hugoresende27@gmail.com>
2021-10-16 20:00:19 +0100

    sab

--- script.js
+++ script.js
@@ -10,11 +10,32 @@ document.addEventListener("DOMContentLoaded", function()
     jogo.msg = document.querySelector(".msg");
     jogo.tent = document.querySelector('input');
     jogo.btn = document.querySelector('button');
+    jogo.btn.addEventListener("click",advinhaNum);
     iniciar();
-})
+});
+
+function advinhaNum()
+{
+    let tempTent = jogo.tent.value;
+    tempTent = parseInt(tempTent);
+    if (isNaN(tempTent))
+    {
+        mensagem('Apenas números inteiros', "#553D67  ")
+    }
+    else if (tempTent === jogo.num)
+    {
+        mensagem('EM CHEIO !!!', "green  ")
+    }
+    else
+    {
+        mensagem('ERROU FEIO !!!', "red  ")
+    }
+    console.log ("A sua tentativa foi "+ tempTent);
+}
 
 function iniciar()
 {
+    jogo.num = ranNumero(jogo.min, jogo.max);
     let tempMsg = "Bem vindo, advinha um número entre "+jogo.min+" e "+jogo.max;
     mensagem(tempMsg , "black");
 
@@ -23,4 +44,9 @@ function mensagem(msg , clr)
 {
     jogo.msg.innerHTML = msg;
     jogo.msg.style.color = clr || "black";
+}
+
+function ranNumero(min,max)
+{                                    //entre o valor min e maximo ranNumero(95,99)= 95,96,97,98,99
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
