16ff9bd783ee223ef03473576db899559929b33d
hugoresende27 <Hugoresende27@gmail.com>
2021-10-16 19:12:15 +0100

    sab

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const res = document.querySelector('.output');
-const msg = document.querySelector(".msg");
-const tent = document.querySelector('input');
-const btn = document.querySelector('button');
+
+let jogo = {'min': 1,
+            'max' : 10};      //jogo é um objeto com os elemento classe output, classe msg, input e button
+
+document.addEventListener("DOMContentLoaded", function()        
+{
+    console.log("Pronto") ;      
+
+    jogo.res = document.querySelector('.output');
+    jogo.msg = document.querySelector(".msg");
+    jogo.tent = document.querySelector('input');
+    jogo.btn = document.querySelector('button');
+    iniciar();
+})
+
+function iniciar()
+{
+    let tempMsg = "Bem vindo, advinha um número entre "+jogo.min+" e "+jogo.max;
+    mensagem(tempMsg , "blue");
+
+}
+function mensagem(msg , clr)
+{
+    jogo.msg.innerHTML = msg;
+    jogo.msg.style.color = clr || "black";
+}
